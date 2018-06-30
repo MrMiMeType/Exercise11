769c9ec3d01675140f216d5aa3f11e2bf7661ad8
166 <xtay04010420>
2018-06-30 16:25:11 +0800

    这是来自166的第二次提交！

--- src/main/java/com/atguigu/TestFree.java
+++ src/main/java/com/atguigu/TestFree.java
@@ -4,7 +4,8 @@ public class TestFree {
 
 	public static void main(String[] args) {
 		System.out.println("这是mrmime的第一次修改！");
-		System.out.println("这是166的第一次修改！");	
+		System.out.println("这是166的第一次修改！");
+		System.out.println("这是166的第三次修改！都没有第二次提交！");
 	}
 
 }
